65569de34cc4aa012f097a3706497e176ea4184e
panboyu1813 <panpeter1813@gmail.com>
2025-04-09 15:09:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def predict():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=13579 ,debug=False)
